0bd3401f36022d94530ca17d7e9153d20f9cbc56
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-07 23:52:00 +0900

    Add train_llama_task_adaptation-min4.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # basic
-mamba create -n GNER python=3.11 -y;
-mamba activate GNER;
-mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y;
-pip install -U -r requirements.txt;
+mamba create -n GNER python=3.11 -y
+mamba activate GNER
+mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y
+pip install -U -r requirements.txt
 
 # train
 bash scripts/train_llama_task_adaptation-min.sh
+bash scripts/train_llama_task_adaptation-min4.sh
+#bash scripts/train_llama_task_adaptation-min8.sh
