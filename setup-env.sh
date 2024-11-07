638fe8b73ce25e54d58d82dab9d8a4b0c958898e
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-08 08:50:06 +0900

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -2,7 +2,8 @@
 # basic
 mamba create -n GNER python=3.11 -y
 mamba activate GNER
-mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y
+mamba install -n GNER cuda-libraries-dev=11.8 cuda-libraries=11.8 cuda-nvcc=11.8 cuda-nvrtc=11.8 cuda-nvrtc-dev=11.8 cuda-runtime=11.8 cuda-cccl=11.8 -c nvidia -c pytorch -y
+#mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y
 pip install -U -r requirements.txt
 
 # train
