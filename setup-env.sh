7cdfeaecda562575810e880db7d634347f3ebac1
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-07 19:12:00 +0900

    Update requirements.txt: latest numpy, torch, deepspeed

--- setup-env.sh
+++ setup-env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # basic
-mamba create -n GNER python=3.11 -y; mamba activate GNER;
+mamba create -n GNER python=3.11 -y;
+mamba activate GNER;
 mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y;
-pip install -r requirements.txt;
+pip install -U -r requirements.txt;
 
 # train
 bash scripts/train_llama_task_adaptation-min.sh
