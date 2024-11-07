deb2dc91b3cc5dfe28be250d2ac94f137dc9f36b
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-07 18:15:25 +0900

    Add train_llama_task_adaptation-min.done

--- scripts/train_llama_task_adaptation-min.sh
+++ scripts/train_llama_task_adaptation-min.sh
@@ -9,7 +9,7 @@ DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 OUTPUT_DIR=output/llama-7b-task-adaptation
 
-DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero1_llama.json
+DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero2_llama.json
 RUN_NAME=llama-7B-experiment
 
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
@@ -19,7 +19,7 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --predict_with_generate \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --data_dir $DATA_DIR \
-    --preprocessing_num_workers 4 \
+    --preprocessing_num_workers 12 \
     --metric_for_best_model "eval_average_f1" \
     --greater_is_better True \
     --train_json_dir $TRAIN_JSON_DIR \
@@ -29,10 +29,11 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 32 \
+    --gradient_checkpointing True \
     --learning_rate 2e-05 \
     --weight_decay 0. \
     --warmup_ratio 0.04 \
-    --num_train_epochs 3 \
+    --num_train_epochs 0.01 \
     --lr_scheduler_type "cosine" \
     --deepspeed $DEEPSPEED_CONFIG \
     --run_name $RUN_NAME \
