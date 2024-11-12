d2fc49bf8f4e3a31c883b8d3ec7b0488d552802b
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-13 01:03:54 +0900

    Update train_t5_xl_task_adaptation.sh

--- scripts/train_t5_xl_task_adaptation.sh
+++ scripts/train_t5_xl_task_adaptation.sh
@@ -9,17 +9,16 @@ DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 OUTPUT_DIR=output/flan-t5-xl-task-adaptation
 DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero0_t5.json
-
 RUN_NAME=flan-t5-xl-experiment
 
 deepspeed --include=="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
+    --bf16 True --tf32 True \
     --do_train \
     --do_predict \
     --predict_with_generate \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --data_dir $DATA_DIR \
-    --preprocessing_num_workers 4 \
-    --load_best_model_at_end True \
+    --preprocessing_num_workers 12 \
     --metric_for_best_model "eval_average_f1" \
     --greater_is_better True \
     --train_json_dir $TRAIN_JSON_DIR \
@@ -31,6 +30,8 @@ deepspeed --include=="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py 
     --gradient_accumulation_steps 16 \
     --gradient_checkpointing True \
     --learning_rate 5e-05 \
+    --lr_scheduler_type constant \
+    --warmup_steps 0 \
     --num_train_epochs 10 \
     --deepspeed $DEEPSPEED_CONFIG \
     --run_name $RUN_NAME \
@@ -39,10 +40,7 @@ deepspeed --include=="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py 
     --generation_max_length 640 \
     --overwrite_output_dir \
     --overwrite_cache \
-    --lr_scheduler_type constant \
-    --warmup_steps 0 \
     --logging_strategy steps \
     --logging_steps 10 \
-    --evaluation_strategy epoch \
     --save_strategy epoch \
     --seed 1234
