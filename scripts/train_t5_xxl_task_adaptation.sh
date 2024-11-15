a2e74175069794fce41e83d4e2125e84ce9f90a0
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-16 04:32:38 +0900

    evaluation_strategy -> eval_strategy

--- scripts/train_t5_xxl_task_adaptation.sh
+++ scripts/train_t5_xxl_task_adaptation.sh
@@ -42,6 +42,6 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --overwrite_cache \
     --logging_strategy steps \
     --logging_steps 10 \
-    --evaluation_strategy no \
+    --eval_strategy no \
     --save_strategy epoch \
     --seed 1234
