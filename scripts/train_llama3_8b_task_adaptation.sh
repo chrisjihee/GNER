64eaae346f0a9cf92257ba740ed3a70100538408
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-15 16:55:00 +0900

    Update train_llama3_8b_task_adaptation.sh

--- scripts/train_llama3_8b_task_adaptation.sh
+++ scripts/train_llama3_8b_task_adaptation.sh
@@ -2,16 +2,16 @@ set -x
 
 port=$(shuf -i25000-30000 -n1)
 
-MODEL_NAME_OR_PATH=meta-llama/Meta-Llama-3-8B
+MODEL_NAME_OR_PATH=meta-llama/Llama-3.1-8B
 DATA_DIR=data
 TRAIN_JSON_DIR=data/pile-ner.json
 DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 OUTPUT_DIR=output/llama3-8b-task-adaptation
-DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero2_llama.json
+DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero3_llama.json
 RUN_NAME=llama3-8B-experiment
 
-# TODO: MODEL_NAME_OR_PATH=meta-llama/Meta-Llama-3-8B -> meta-llama/Meta-Llama-3-8B-Instruct, meta-llama/Llama-3.1-8B
+# TODO: MODEL_NAME_OR_PATH=meta-llama/Llama-3.1-8B -> meta-llama/Llama-3.1-8B-Instruct
 # TODO: generation_max_length 1280 -> 640
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --bf16 True --tf32 True \
