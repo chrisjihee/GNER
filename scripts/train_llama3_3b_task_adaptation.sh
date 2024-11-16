39323ba334179abd193e76e7bd78cf7d37d2aa1a
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-16 23:55:44 +0900

    --generation_max_length 640

--- scripts/train_llama3_3b_task_adaptation.sh
+++ scripts/train_llama3_3b_task_adaptation.sh
@@ -8,11 +8,9 @@ TRAIN_JSON_DIR=data/pile-ner.json
 DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 OUTPUT_DIR=output/llama3-3b-task-adaptation
-DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero2_llama.json  # TODO zero2 -> zero0
+DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero0_llama.json  # TODO zero2 -> zero0
 RUN_NAME=llama3-3B-experiment
 
-# TODO: num_train_epochs 3 -> 10
-# TODO: generation_max_length 1280 -> 640
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --bf16 True --tf32 True \
     --do_train \
@@ -38,7 +36,7 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --run_name $RUN_NAME \
     --max_source_length 640 \
     --max_target_length 640 \
-    --generation_max_length 1280 \
+    --generation_max_length 640 \
     --overwrite_output_dir \
     --logging_strategy steps \
     --logging_steps 10 \
