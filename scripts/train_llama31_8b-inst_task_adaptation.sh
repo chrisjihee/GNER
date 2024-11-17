4600a20ca0d50f8319870e42e23fb68f4c93ff51
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-17 16:20:04 +0900

    DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero3_llama.json

--- scripts/train_llama31_8b-inst_task_adaptation.sh
+++ scripts/train_llama31_8b-inst_task_adaptation.sh
@@ -1,16 +1,13 @@
 set -x
-
 port=$(shuf -i25000-30000 -n1)
-
-MODEL_NAME_OR_PATH=meta-llama/Llama-3.1-8B-Instruct
 DATA_DIR=data
 TRAIN_JSON_DIR=data/pile-ner.json
-DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
+DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
+DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero3_llama.json
+MODEL_NAME_OR_PATH=meta-llama/Llama-3.1-8B-Instruct
 OUTPUT_DIR=output/llama31-8b-inst-task-adaptation
-DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero2_llama.json
 RUN_NAME=llama31-8B-inst-experiment
-
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --bf16 True --tf32 True \
     --do_train \
