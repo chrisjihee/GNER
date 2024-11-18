64873cff1d317656de88598c1ef342cac28e1abf
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-18 13:30:47 +0900

    Update train_t5_base_task_adaptation.sh

--- scripts/train_t5_base_task_adaptation.sh
+++ scripts/train_t5_base_task_adaptation.sh
@@ -1,16 +1,13 @@
 set -x
-
 port=$(shuf -i25000-30000 -n1)
-
-MODEL_NAME_OR_PATH=google/flan-t5-base
 DATA_DIR=data
 TRAIN_JSON_DIR=data/pile-ner.json
-DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
-OUTPUT_DIR=output/flan-t5-base-task-adaptation
+DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero0_t5.json
+MODEL_NAME_OR_PATH=google/flan-t5-base
+OUTPUT_DIR=output/flan-t5-base-task-adaptation
 RUN_NAME=flan-t5-base-experiment
-
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --bf16 True --tf32 True \
     --do_train \
@@ -30,7 +27,7 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --lr_scheduler_type constant \
     --learning_rate 5e-05 \
     --warmup_steps 0 \
-    --num_train_epochs 12 \
+    --num_train_epochs 1 \
     --deepspeed $DEEPSPEED_CONFIG \
     --run_name $RUN_NAME \
     --max_source_length 640 \
