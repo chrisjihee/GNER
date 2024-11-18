bfc05dd10c94679d00d6c88fda4d8df23ed6b088
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-18 21:02:03 +0900

    Save eval_t5_task_adaptation_json_input.out

--- scripts/eval_t5_task_adaptation_json_input.sh
+++ scripts/eval_t5_task_adaptation_json_input.sh
@@ -1,9 +1,6 @@
 set -x
 port=$(shuf -i25000-30000 -n1)
 TEST_JSON_DIR=data/zero-shot-test.jsonl
-DATA_DIR=data
-DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
-INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 MODEL_NAME_OR_PATH=output/flan-t5-base-task-adaptation-1/checkpoint-4956
 BEAM_SIZE=1
 OUTPUT_DIR=output/flan-t5-xxl-task-adaptation-beam${BEAM_SIZE}
@@ -13,14 +10,11 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --test_json_dir $TEST_JSON_DIR \
     --no_load_gner_customized_datasets \
-    --data_dir $DATA_DIR \
-    --data_config_dir $DATA_CONFIG_DIR \
-    --instruction_file $INSTRUCTION_FILE \
     --output_dir $OUTPUT_DIR \
     --run_name $RUN_NAME \
     --generation_num_beams ${BEAM_SIZE} \
     --preprocessing_num_workers 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_eval_batch_size 32 \
     --bf16 True --tf32 True \
     --max_source_length 640 \
     --max_target_length 640 \
