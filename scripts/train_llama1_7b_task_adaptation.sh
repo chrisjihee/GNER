4c23f641cf647d30e2239eabe46aa486467a17b9
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-18 16:01:29 +0900

    Clean scripts

--- scripts/train_llama1_7b_task_adaptation.sh
+++ scripts/train_llama1_7b_task_adaptation.sh
@@ -2,12 +2,12 @@ set -x
 port=$(shuf -i25000-30000 -n1)
 DATA_DIR=data
 TRAIN_JSON_DIR=data/pile-ner.json
-INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
+INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero2_llama.json
 MODEL_NAME_OR_PATH=yahma/llama-7b-hf
-OUTPUT_DIR=output/llama-7b-task-adaptation
-RUN_NAME=llama-7B-experiment
+OUTPUT_DIR=output/llama1-7b-task-adaptation
+RUN_NAME=llama1-7B-experiment
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --bf16 True --tf32 True \
     --do_train \
@@ -15,12 +15,12 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --predict_with_generate \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --data_dir $DATA_DIR \
-    --preprocessing_num_workers 12 \
+    --preprocessing_num_workers 4 \
     --train_json_dir $TRAIN_JSON_DIR \
     --data_config_dir $DATA_CONFIG_DIR \
     --instruction_file $INSTRUCTION_FILE \
     --output_dir $OUTPUT_DIR \
-    --per_device_eval_batch_size 40 \
+    --per_device_eval_batch_size 32 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 32 \
     --gradient_checkpointing True \
