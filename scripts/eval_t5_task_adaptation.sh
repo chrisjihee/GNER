e4435aa64957b9ea12fc14b3b2c800550b575ec1
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-18 20:35:56 +0900

    Sync eval_t5_task_adaptation.sh with train_t5_base_task_adaptation.sh

--- scripts/eval_t5_task_adaptation.sh
+++ scripts/eval_t5_task_adaptation.sh
@@ -1,29 +1,24 @@
 set -x
-
 port=$(shuf -i25000-30000 -n1)
-
-BEAM_SIZE=1
-MODEL_NAME_OR_PATH=output/flan-t5-xxl-task-adaptation/checkpoint-xxx
 DATA_DIR=data
 DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
+MODEL_NAME_OR_PATH=output/flan-t5-base-task-adaptation/checkpoint-480
+BEAM_SIZE=1
 OUTPUT_DIR=output/flan-t5-xxl-task-adaptation-beam${BEAM_SIZE}
-
 RUN_NAME=flan-t5-xxl-experiment
-
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
-    --bf16 True --tf32 True \
+    --do_predict --predict_with_generate \
     --generation_num_beams ${BEAM_SIZE} \
-    --do_predict \
-    --predict_with_generate \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --data_dir $DATA_DIR \
     --preprocessing_num_workers 4 \
     --data_config_dir $DATA_CONFIG_DIR \
     --instruction_file $INSTRUCTION_FILE \
     --output_dir $OUTPUT_DIR \
-    --per_device_eval_batch_size 4 \
     --run_name $RUN_NAME \
+    --per_device_eval_batch_size 4 \
+    --bf16 True --tf32 True \
     --max_source_length 640 \
     --max_target_length 640 \
     --generation_max_length 640 \
