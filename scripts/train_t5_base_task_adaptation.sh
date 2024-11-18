16501dee1516511f4f39d95b0ed26949c8647370
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-18 18:35:08 +0900

    Updating train_t5_base_task_adaptation.sh

--- scripts/train_t5_base_task_adaptation.sh
+++ scripts/train_t5_base_task_adaptation.sh
@@ -34,10 +34,17 @@ deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --max_target_length 640 \
     --generation_max_length 640 \
     --overwrite_output_dir \
-    --overwrite_cache \
-    --logging_strategy steps \
     --logging_steps 10 \
-    --eval_strategy no \
-    --save_strategy no \
+    --logging_strategy steps \
+    --save_strategy epoch \
+    --eval_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model eval_average_f1 \
+    --greater_is_better True \
     --seed 1234
-# + deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 28991 src/run.py --bf16 True --tf32 True --do_train --do_predict --predict_with_generate --model_name_or_path google/flan-t5-base --data_dir data --preprocessing_num_workers 4 --train_json_dir data/pile-ner.json --data_config_dir configs/dataset_configs/task_adaptation_configs --instruction_file configs/instruction_configs/instruction.json --output_dir output/flan-t5-base-task-adaptation --per_device_eval_batch_size 32 --per_device_train_batch_size 32 --gradient_accumulation_steps 1 --gradient_checkpointing True --lr_scheduler_type constant --learning_rate 5e-05 --warmup_steps 0 --num_train_epochs 1 --deepspeed configs/deepspeed_configs/deepspeed_zero0_t5.json --run_name flan-t5-base-experiment --max_source_length 640 --max_target_length 640 --generation_max_length 640 --overwrite_output_dir --overwrite_cache --logging_strategy steps --logging_steps 10 --eval_strategy no --save_strategy no --seed 1234
+
+# + deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 26922 src/run.py --bf16 True --tf32 True --do_train --do_predict --predict_with_generate --model_name_or_path google/flan-t5-base --data_dir data --preprocessing_num_workers 4 --train_json_dir data/pile-ner.json --data_config_dir configs/dataset_configs/task_adaptation_configs --instruction_file configs/instruction_configs/instruction.json --output_dir output/flan-t5-base-task-adaptation --per_device_eval_batch_size 32 --per_device_train_batch_size 32 --gradient_accumulation_steps 1 --gradient_checkpointing True --lr_scheduler_type constant --learning_rate 5e-05 --warmup_steps 0 --num_train_epochs 1 --deepspeed configs/deepspeed_configs/deepspeed_zero0_t5.json --run_name flan-t5-base-experiment --max_source_length 640 --max_target_length 640 --generation_max_length 640 --overwrite_output_dir --logging_steps 10 --logging_strategy steps --save_strategy epoch --eval_strategy epoch --load_best_model_at_end True --metric_for_best_model eval_average_f1 --greater_is_better True --seed 1234
+
+#    --overwrite_cache \
+#    --eval_strategy no \
+#    --save_strategy no \
