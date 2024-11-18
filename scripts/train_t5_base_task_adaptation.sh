aeb40d1fd327a622c5e806050aa29571d184a221
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-18 20:47:07 +0900

    Sync eval_t5_task_adaptation_json_input.sh

--- scripts/train_t5_base_task_adaptation.sh
+++ scripts/train_t5_base_task_adaptation.sh
@@ -1,7 +1,7 @@
 set -x
 port=$(shuf -i25000-30000 -n1)
-DATA_DIR=data
 TRAIN_JSON_DIR=data/pile-ner.json
+DATA_DIR=data
 DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
 DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero0_t5.json
@@ -11,13 +11,13 @@ RUN_NAME=flan-t5-base-experiment
 deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
     --do_train --do_predict --predict_with_generate \
     --model_name_or_path $MODEL_NAME_OR_PATH \
-    --data_dir $DATA_DIR \
-    --preprocessing_num_workers 4 \
     --train_json_dir $TRAIN_JSON_DIR \
+    --data_dir $DATA_DIR \
     --data_config_dir $DATA_CONFIG_DIR \
     --instruction_file $INSTRUCTION_FILE \
     --output_dir $OUTPUT_DIR \
     --run_name $RUN_NAME \
+    --preprocessing_num_workers 4 \
     --per_device_eval_batch_size 32 \
     --per_device_train_batch_size 32 \
     --gradient_accumulation_steps 1 \
