548afbed66fe2c81db66c34406f58ab86860309d
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-20 09:15:06 +0900

    Save scripts

---
+++ scripts/train_llama3_3b_supervised-plus.sh
@@ -0,0 +1,34 @@
+set -x
+port=$(shuf -i25000-30000 -n1)
+TRAIN_JSON_DIR=data/zero-shot-train-plus.jsonl
+VALID_JSON_DIR=data/zero-shot-test.jsonl
+MODEL_NAME_OR_PATH=meta-llama/Llama-3.2-3B
+OUTPUT_DIR=output/llama3-3B-supervised
+RUN_NAME=llama3-3B-supervised
+DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero0_llama.json
+deepspeed --include="localhost:0,1,2,3,4,5,6,7" --master_port $port src/run.py \
+    --do_train --do_eval --predict_with_generate \
+    --train_json_dir $TRAIN_JSON_DIR \
+    --valid_json_dir $VALID_JSON_DIR \
+    --no_load_gner_customized_datasets \
+    --model_name_or_path $MODEL_NAME_OR_PATH \
+    --output_dir $OUTPUT_DIR \
+    --run_name $RUN_NAME \
+    --preprocessing_num_workers 4 \
+    --per_device_eval_batch_size 32 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 4 \
+    --gradient_checkpointing True \
+    --bf16 True --tf32 True \
+    --lr_scheduler_type cosine \
+    --learning_rate 2e-05 \
+    --warmup_ratio 0.04 --weight_decay 0. \
+    --num_train_epochs 3 \
+    --max_source_length 640 \
+    --max_target_length 640 \
+    --generation_max_length 1280 \
+    --logging_strategy steps --logging_steps 10 \
+    --eval_strategy epoch \
+    --save_strategy no \
+    --overwrite_output_dir --overwrite_cache \
+    --seed 1234 --deepspeed $DEEPSPEED_CONFIG
