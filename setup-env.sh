30830852d1b5f7214c94f7a5eed5661c82636012
Jihee Ryu <chrisjihee@etri.re.kr>
2025-01-10 12:14:52 +0900

    python=3.12

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # basic
-mamba create -n GNER python=3.11 -y; mamba activate GNER
+mamba create -n GNER python=3.12 -y; mamba activate GNER
 mamba install -n GNER cuda-libraries=11.8 cuda-libraries-dev=11.8 cuda-cudart=11.8 cuda-cudart-dev=11.8 \
                       cuda-nvrtc=11.8 cuda-nvrtc-dev=11.8 cuda-driver-dev=11.8 \
                       cuda-nvcc=11.8 cuda-cccl=11.8 cuda-runtime=11.8 cuda-version=12.4 \
