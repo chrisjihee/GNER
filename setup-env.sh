44b0b6c950929b65bd06b56d6792017b7ed450cd
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-19 00:23:41 +0900

    Update setup-env.sh: mamba -> conda

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # basic
-mamba create -n GNER python=3.12 -y; mamba activate GNER
-mamba install -n GNER cuda-libraries=11.8 cuda-libraries-dev=11.8 cuda-cudart=11.8 cuda-cudart-dev=11.8 \
+conda install -n base -c conda-forge conda=25.1.1 -y;
+conda create -n GNER python=3.12 -y; conda activate GNER
+conda install -n GNER cuda-libraries=11.8 cuda-libraries-dev=11.8 cuda-cudart=11.8 cuda-cudart-dev=11.8 \
                       cuda-nvrtc=11.8 cuda-nvrtc-dev=11.8 cuda-driver-dev=11.8 \
                       cuda-nvcc=11.8 cuda-cccl=11.8 cuda-runtime=11.8 cuda-version=12.4 \
                       libcusparse=11 libcusparse-dev=11 libcublas=11 libcublas-dev=11 \
@@ -10,13 +11,12 @@ pip install -U -r requirements.txt
 DS_BUILD_FUSED_ADAM=1 DS_BUILD_CPU_ADAM=1 pip install --no-cache deepspeed==0.13.1
 
 # check
-mamba list cuda; mamba list libcu;
+conda list cuda; conda list libcu;
 ds_report
+huggingface-cli whoami
 
 # option
-#ln -s ~/.cache/huggingface .cache_hf
+ln -s ~/.cache/huggingface .cache_hf
 
 # train
-#bash scripts/train_llama_task_adaptation-min4.sh
-#bash scripts/train_llama_task_adaptation-min8.sh
 bash scripts/train_llama_task_adaptation-full.sh
