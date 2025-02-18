6509cb9c8cbc833f55c226fe27bc0951b65b5127
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-19 03:08:17 +0900

    Save data/Ontonotes

--- dataset-HF.py
+++ dataset-HF.py
@@ -24,7 +24,7 @@ def save_conll_format(dataset_split, output_file, label_names):
     with open(output_file, "w", encoding="utf-8") as f:
         for example in dataset_split:
             tokens = example["tokens"]
-            tags = example["ner_tags"]
+            tags = example["ner_tags"] if "ner_tags" in example else example["tags"]
             for token, tag_id in zip(tokens, tags):
                 # Convert the numerical tag ID to its string label
                 label = label_names[tag_id]
@@ -33,7 +33,7 @@ def save_conll_format(dataset_split, output_file, label_names):
             f.write("\n")
 
 
-def main(dataset_name, output_dir):
+def main(dataset_name, output_dir, label2id=None):
     print("=" * 80)
     print(f"Saving the '{dataset_name}' dataset to '{output_dir}'...")
     output_dir = Path(output_dir)
@@ -43,7 +43,12 @@ def main(dataset_name, output_dir):
     dataset = load_dataset(dataset_name, trust_remote_code=True)
 
     # Retrieve the list of label names
-    label_names = dataset["train"].features["ner_tags"].feature.names
+    if label2id:
+        label_names = [None] * len(label2id)
+        for label, id in label2id.items():
+            label_names[id] = label
+    else:
+        label_names = dataset["train"].features["ner_tags"].feature.names
 
     # Save the label list to label.txt
     with open(output_dir / "label.txt", "w", encoding="utf-8") as f:
@@ -61,10 +66,51 @@ def main(dataset_name, output_dir):
     print(f"Number of test samples: {len(dataset['test'])}")
 
 
+ontonotes5_label2id = {
+    "O": 0,
+    "B-CARDINAL": 1,
+    "B-DATE": 2,
+    "I-DATE": 3,
+    "B-PERSON": 4,
+    "I-PERSON": 5,
+    "B-NORP": 6,
+    "B-GPE": 7,
+    "I-GPE": 8,
+    "B-LAW": 9,
+    "I-LAW": 10,
+    "B-ORG": 11,
+    "I-ORG": 12,
+    "B-PERCENT": 13,
+    "I-PERCENT": 14,
+    "B-ORDINAL": 15,
+    "B-MONEY": 16,
+    "I-MONEY": 17,
+    "B-WORK_OF_ART": 18,
+    "I-WORK_OF_ART": 19,
+    "B-FAC": 20,
+    "B-TIME": 21,
+    "I-CARDINAL": 22,
+    "B-LOC": 23,
+    "B-QUANTITY": 24,
+    "I-QUANTITY": 25,
+    "I-NORP": 26,
+    "I-LOC": 27,
+    "B-PRODUCT": 28,
+    "I-TIME": 29,
+    "B-EVENT": 30,
+    "I-EVENT": 31,
+    "I-FAC": 32,
+    "B-LANGUAGE": 33,
+    "I-PRODUCT": 34,
+    "I-ORDINAL": 35,
+    "I-LANGUAGE": 36
+}
+
 if __name__ == "__main__":
     # main("ghadeermobasher/BC5CDR-Chemical-Disease", "data/bc5cdr")
     # main("chintagunta85/bc4chemd", "data/bc4chemd")
     # main("strombergnlp/broad_twitter_corpus", "data/broad_twitter_corpus")
     # main("eriktks/conll2003", "data/conll2003")
     # main("DFKI-SLT/fabner", "data/FabNER")
-    main("ncbi/ncbi_disease", "data/ncbi")
+    # main("ncbi/ncbi_disease", "data/ncbi")
+    main("tner/ontonotes5", "data/Ontonotes", label2id=ontonotes5_label2id)
