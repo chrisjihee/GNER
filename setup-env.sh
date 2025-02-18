48a91908e04ba06fa7587a6b3643632abcad6d65
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-19 00:52:30 +0900

    Update for data

--- setup-env.sh
+++ setup-env.sh
@@ -18,5 +18,11 @@ huggingface-cli whoami
 # option
 ln -s ~/.cache/huggingface .cache_hf
 
+# data
+cd data; gzip -d -k pile-ner.json.gz; cd ..
+cd data; gzip -d -k pile-ner.jsonl.gz; cd ..
+
 # train
+screen -h 1000000 -R GNER
+conda activate GNER
 bash scripts/train_t5_large_task_adaptation.sh
