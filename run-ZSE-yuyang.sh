d96f386afaf8e9c58bed3e181c6b57980092cbf2
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-24 01:44:17 +0900

    Update script names: yuyang and jihee

--- run-ZSE-yuyang.sh
+++ run-ZSE-yuyang.sh
@@ -2,4 +2,4 @@ eval "$(conda shell.bash hook)"
 conda activate GNER
 cd ~/proj/GNER
 rm -rf .cache_hf/datasets
-python "scripts/GNER-SFT-BL.py" &> "output/GNER-SFT-BL-$(hostname).out"
+bash "scripts/ZSE-yuyang-T5-Large.sh" &> "output/ZSE-yuyang-T5-Large-$(hostname).out"
