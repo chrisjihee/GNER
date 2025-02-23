103b8218d3df9a3a7caeea41a07b39fea310a573
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-23 18:16:17 +0900

    per_device_train_batch_size=2, gradient_accumulation_steps=16

--- scripts/SFT-T5-Large.sh
+++ scripts/SFT-T5-Large.sh
@@ -23,9 +23,9 @@ deepspeed --include="localhost:$CUDA_VISIBLE_DEVICES" --master_port $MASTER_PORT
     --instruction_file $INSTRUCTION_FILE \
     --data_config_dir $DATA_CONFIG_DIR \
     --output_dir $OUTPUT_DIR \
-    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 16 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 8 \
     --learning_rate 5e-05 \
     --num_train_epochs 10 \
     --deepspeed $DEEPSPEED_CONFIG \
