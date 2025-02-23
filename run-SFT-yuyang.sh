e751707938809584160cb899fb9c3d5d707df4a8
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-24 02:20:11 +0900

    Sync scripts: flan-t5-base

--- run-SFT-yuyang.sh
+++ run-SFT-yuyang.sh
@@ -2,4 +2,4 @@ eval "$(conda shell.bash hook)"
 conda activate GNER
 cd ~/proj/GNER
 rm -rf .cache_hf/datasets
-bash "scripts/SFT-yuyang-T5-Large.sh" &> "output/SFT-yuyang-T5-Large-$(hostname).out"
+bash "scripts/SFT-yuyang-BL.sh" &> "output/SFT-yuyang-BL-$(hostname).out"
--- run-ZSE-yuyang.sh
+++ run-ZSE-yuyang.sh
@@ -2,4 +2,4 @@ eval "$(conda shell.bash hook)"
 conda activate GNER
 cd ~/proj/GNER
 rm -rf .cache_hf/datasets
-bash "scripts/ZSE-yuyang-T5-Large.sh" &> "output/ZSE-yuyang-T5-Large-$(hostname).out"
+bash "scripts/ZSE-yuyang-BL.sh" &> "output/ZSE-yuyang-BL-$(hostname).out"
