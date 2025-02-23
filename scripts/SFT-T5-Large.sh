e387b83c1044bc5a5f4f75ab3dfacc47923abbb5
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-23 18:09:53 +0900

    Sync SFT-T5-Large.sh and ZSE-T5-Large.sh

--- scripts/SFT-T5-Large.sh
+++ scripts/SFT-T5-Large.sh
@@ -1,18 +1,16 @@
 set -x
-
-port=$(shuf -i25000-30000 -n1)
-
-MODEL_NAME_OR_PATH=google/flan-t5-large
+CUDA_VISIBLE_DEVICES=4,5,6,7
+MASTER_PORT=$(shuf -i25000-30000 -n1)
 DATA_DIR=data
-TRAIN_JSON_DIR=data/pile-ner.json
-DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
-INSTRUCTION_FILE=configs/instruction_configs/instruction.json
-OUTPUT_DIR=output/flan-t5-large-task-adaptation
-DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero0_t5.json
+INSTRUCTION_FILE=configs/instruction/GNER-paper.json
+DEEPSPEED_CONFIG=configs/deepspeed/ds2_t5.json
+MODEL_NAME_OR_PATH=google/flan-t5-large
+OUTPUT_DIR=output/SFT-T5-Large
+RUN_NAME=SFT-T5-Large
+DATA_CONFIG_DIR=configs/dataset/SFT
 
-RUN_NAME=flan-t5-large-experiment
 
-deepspeed --include="localhost:0,1,2,3" --master_port $port gner/run.py \
+deepspeed --include="localhost:$CUDA_VISIBLE_DEVICES" --master_port $MASTER_PORT gner/run.py \
     --do_train \
     --do_predict \
     --predict_with_generate \
@@ -22,9 +20,8 @@ deepspeed --include="localhost:0,1,2,3" --master_port $port gner/run.py \
     --load_best_model_at_end True \
     --metric_for_best_model "eval_average_f1" \
     --greater_is_better True \
-    --train_json_dir $TRAIN_JSON_DIR \
-    --data_config_dir $DATA_CONFIG_DIR \
     --instruction_file $INSTRUCTION_FILE \
+    --data_config_dir $DATA_CONFIG_DIR \
     --output_dir $OUTPUT_DIR \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 8 \
