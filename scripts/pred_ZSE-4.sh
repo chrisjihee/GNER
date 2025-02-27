6d6bf193a38e50460273c984d37bf1cd2fb79b70
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-28 01:32:50 +0900

    Update device!

--- scripts/pred_ZSE-4.sh
+++ scripts/pred_ZSE-4.sh
@@ -1,2 +1,2 @@
-python predict.py --device=cuda:1 --num_generation=100 --generation_by_sample --temperature=5.0 --top_p=0.9 --input_file=data/ZSE-validation.jsonl
-python predict.py --device=cuda:1 --num_generation=100 --generation_by_sample --temperature=5.0 --top_p=0.9 --input_file=data/ZSE-test.jsonl
+python predict.py --device=cuda:3 --num_generation=100 --generation_by_sample --temperature=2.0 --top_p=0.9 --input_file=data/ZSE-validation.jsonl
+python predict.py --device=cuda:3 --num_generation=100 --generation_by_sample --temperature=2.0 --top_p=0.9 --input_file=data/ZSE-test.jsonl
--- scripts/pred_ZSE-7.sh
+++ scripts/pred_ZSE-7.sh
@@ -1,2 +1,2 @@
-python predict.py --device=cuda:1 --num_generation=100 --generation_by_sample --temperature=10.0 --top_p=0.8 --input_file=data/ZSE-validation.jsonl
-python predict.py --device=cuda:1 --num_generation=100 --generation_by_sample --temperature=10.0 --top_p=0.8 --input_file=data/ZSE-test.jsonl
+python predict.py --device=cuda:6 --num_generation=100 --generation_by_sample --temperature=3.0 --top_p=0.8 --input_file=data/ZSE-validation.jsonl
+python predict.py --device=cuda:6 --num_generation=100 --generation_by_sample --temperature=3.0 --top_p=0.8 --input_file=data/ZSE-test.jsonl
