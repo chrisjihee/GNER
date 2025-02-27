e82c53ee3cb92df9c5359d4801ec7523cca1ccd9
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-28 01:20:42 +0900

    Add pred_ZSE-1.sh pred_ZSE-2.sh pred_ZSE-3.sh pred_ZSE-4.sh pred_ZSE-5.sh pred_ZSE-6.sh pred_ZSE-7.sh pred_ZSE-8.sh

---
+++ scripts/pred_ZSE-1.sh
@@ -0,0 +1,2 @@
+python predict.py --device=cuda:0 --num_generation=100 --generation_by_beam --input_file=data/ZSE-validation.jsonl
+python predict.py --device=cuda:0 --num_generation=100 --generation_by_beam --input_file=data/ZSE-test.jsonl
---
+++ scripts/pred_ZSE-8.sh
@@ -0,0 +1,2 @@
+python predict.py --device=cuda:1 --num_generation=100 --generation_by_sample --temperature=10.0 --top_p=0.7 --input_file=data/ZSE-validation.jsonl
+python predict.py --device=cuda:1 --num_generation=100 --generation_by_sample --temperature=10.0 --top_p=0.7 --input_file=data/ZSE-test.jsonl
