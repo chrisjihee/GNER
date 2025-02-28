7b5ea7c8c69aaf6e5862a74e5acae3db4101fdd7
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-28 17:56:06 +0900

    Add PerformanceMetrics, find_increasing_indices()

--- predict.py
+++ predict.py
@@ -1,16 +1,19 @@
-import datasets
 import logging
 from pathlib import Path
+from typing import List
 
+import datasets
 import torch
 import typer
 from datasets import load_dataset
+from pydantic import BaseModel
 from typing_extensions import Annotated
 
 from chrisbase.data import AppTyper, JobTimer, NewProjectEnv
 from chrisbase.io import LoggingFormat, LoggerWriter, new_path
 from chrisbase.time import from_timestamp, now_stamp
 from chrisdata.ner import GenNERSampleWrapper
+from gner import extract_predictions3, parser
 from progiter import ProgIter
 from transformers import (
     AutoTokenizer,
@@ -23,8 +26,8 @@ logger: logging.Logger = logging.getLogger("gner")
 
 def predict(
         device: Annotated[str, typer.Option("--device")] = ...,
-        pretrained: Annotated[str, typer.Option("--pretrained")] = "dyyyyyyyy/GNER-T5-base",  # "dyyyyyyyy/GNER-T5-large", "output-lfs/ZSE-jihee-BL-dl012/FlanT5-Base-BL/checkpoint-9900", "output-lfs/ZSE-yuyang-BL-lirs-b1/checkpoint-9900"
         input_file: Annotated[str, typer.Option("--input_file")] = ...,  # "data/ZSE-validation.jsonl", "data/ZSE-test.jsonl"
+        pretrained: Annotated[str, typer.Option("--pretrained")] = "dyyyyyyyy/GNER-T5-base",  # "dyyyyyyyy/GNER-T5-large", "output-lfs/ZSE-jihee-BL-dl012/FlanT5-Base-BL/checkpoint-9900", "output-lfs/ZSE-yuyang-BL-lirs-b1/checkpoint-9900"
         max_generation_tokens: Annotated[int, typer.Option("--max_generation_tokens")] = 640,
         generation_by_sample: Annotated[bool, typer.Option("--generation_by_sample/--generation_by_beam")] = ...,
         generation_num_return: Annotated[int, typer.Option("--num_generation")] = ...,
@@ -88,8 +91,36 @@ def predict(
         logger.info(f"Saved predictions to {(env.output_dir / env.output_file)}: #example={num_example_outputs}, #prediction={num_prediction_outputs}")
 
 
+class PerformanceMetrics(BaseModel):
+    f1: float = 0.0
+    prec: float = 0.0
+    rec: float = 0.0
+    n_correct: int = 0
+    n_pos_gold: int = 0
+    n_pos_pred: int = 0
+
+    def __str__(self):
+        return f"F1={self.f1:.4f}, Prec={self.prec:.4f}, Rec={self.rec:.4f}, #correct={self.n_correct}, #pos_gold={self.n_pos_gold}, #pos_pred={self.n_pos_pred}"
+
+
+def find_increasing_indices(lst):
+    if not lst:
+        return []
+
+    indices = []
+    prev_value = lst[0]
+
+    for i in range(1, len(lst)):
+        if lst[i] > prev_value:
+            indices.append(i)
+            prev_value = lst[i]  # Update previous value to the new threshold
+
+    return indices
+
+
 def evaluate(
         predction_file: Annotated[str, typer.Option("--predction_file")] = "output/ZSE-predict/ZSE-test-pred-by_beam-num=100.jsonl",  # "output/ZSE-predict/ZSE-test-pred-by_beam-num=100.jsonl"
+        pretrained: Annotated[str, typer.Option("--pretrained")] = "dyyyyyyyy/GNER-T5-base",  # "dyyyyyyyy/GNER-T5-large", "output-lfs/ZSE-jihee-BL-dl012/FlanT5-Base-BL/checkpoint-9900", "output-lfs/ZSE-yuyang-BL-lirs-b1/checkpoint-9900"
         output_home: Annotated[str, typer.Option("--output_home")] = "output",
         output_name: Annotated[str, typer.Option("--output_name")] = "ZSE-predict",
         output_file: Annotated[str, typer.Option("--output_file")] = "eval.jsonl",
@@ -113,7 +144,9 @@ def evaluate(
 
     with JobTimer(f"python {env.current_file} {' '.join(env.command_args)}", rt=1, rb=1, rc='=', verbose=verbose):
         input_dataset = load_dataset("json", data_files=str(predction_file), split=datasets.Split.TRAIN)
-        input_dataset = input_dataset.select(range(1))
+        # input_dataset = input_dataset.select(range(10))
+        tokenizer = AutoTokenizer.from_pretrained(pretrained)
+
         logger.info(f"Evaluating predictions from {predction_file} for {len(input_dataset)} samples")
         with (
             ProgIter(input_dataset, desc=f"Evaluating:", stream=LoggerWriter(logger), verbose=2) as progress,
@@ -123,7 +156,51 @@ def evaluate(
             num_prediction_outputs = 0
             for example in progress:
                 example = GenNERSampleWrapper.model_validate(example)
-                logger.info(f"Example: {example.instance}")
+                assert example.instance.prediction_outputs and len(example.instance.prediction_outputs) > 0, f"Missing prediction outputs for {example.instance.id}"
+                # logger.info(f"Answer: {example.instance.prompt_labels}")
+                achievable_perfs: List[PerformanceMetrics] = []
+                for prediction_output in example.instance.prediction_outputs:
+                    n_correct, n_pos_gold, n_pos_pred = 0, 0, 0
+                    # logger.info(f"= prediction_output: {prediction_output}")
+                    words = example.instance.words
+                    labels = example.instance.labels
+                    predictions = extract_predictions3(prediction_output, example=example, tokenizer=tokenizer)
+                    gold_tuples = parser(words, labels)
+                    pred_tuples = parser(words, predictions)
+                    for t in pred_tuples:
+                        if t in gold_tuples:
+                            n_correct += 1
+                        n_pos_pred += 1
+                    n_pos_gold += len(gold_tuples)
+                    prec = n_correct / (n_pos_pred + 1e-10)
+                    rec = n_correct / (n_pos_gold + 1e-10)
+                    f1 = 2 * prec * rec / (prec + rec + 1e-10)
+                    curr_perf = PerformanceMetrics(
+                        f1=f1,
+                        prec=prec,
+                        rec=rec,
+                        n_correct=n_correct,
+                        n_pos_gold=n_pos_gold,
+                        n_pos_pred=n_pos_pred,
+                    )
+                    if not achievable_perfs:
+                        achievable_perf = curr_perf
+                    else:
+                        prev_perf = achievable_perfs[-1]
+                        achievable_perf = curr_perf if curr_perf.f1 > prev_perf.f1 else prev_perf
+                    achievable_perfs.append(achievable_perf)
+
+                    # logger.info(f"  - words : {words}")
+                    # logger.info(f"  - labels: {labels}")
+                    # logger.info(f"  - preds : {predictions}")
+                    # logger.info(f"  - gold_tuples: {gold_tuples}")
+                    # logger.info(f"  - pred_tuples: {pred_tuples}")
+                    # logger.info(f"  - achievable: {achievable_perf}")
+                    # logger.info(f"  - currently : {curr_perf}")
+                    # logger.info("")
+
+                inc_points = find_increasing_indices([x.f1 for x in achievable_perfs])
+                logger.info(f"{len(inc_points)}: {inc_points}: {[f'{x.f1:.4f}' for x in achievable_perfs]}")
 
 
 if __name__ == "__main__":
