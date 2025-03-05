f1acc3c2e253d887a0f3836de00f9e3252b3f25e
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-05 18:15:14 +0900

    Update scripts/pred_ZSE-*.sh (predict command)

--- scripts/pred_ZSE-6.sh
+++ scripts/pred_ZSE-6.sh
@@ -1,2 +1,2 @@
-python predict.py --device=cuda:5 --num_generation=100 --generation_by_sample --temperature=0.7 --top_p=0.7 --input_file=data/ZSE-validation.jsonl
-python predict.py --device=cuda:5 --num_generation=100 --generation_by_sample --temperature=0.7 --top_p=0.7 --input_file=data/ZSE-test.jsonl
+python predict.py predict --device=cuda:5 --num_generation=100 --generation_by_sample --temperature=0.7 --top_p=0.7 --input_file=data/ZSE-validation.jsonl
+python predict.py predict --device=cuda:5 --num_generation=100 --generation_by_sample --temperature=0.7 --top_p=0.7 --input_file=data/ZSE-test.jsonl
