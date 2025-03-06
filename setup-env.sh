e8a4356f10532a5dd7d1a87cf18bc5f30f10b9dd
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-07 00:45:56 +0900

    Minimize conda install cuda*

--- setup-env.sh
+++ setup-env.sh
@@ -12,20 +12,19 @@ conda install -n base -c conda-forge conda=25.1.1 -y;
 conda create -n GNER python=3.12 -y; conda activate GNER
 conda install -n GNER cuda-libraries=11.8 cuda-libraries-dev=11.8 cuda-cudart=11.8 cuda-cudart-dev=11.8 \
                       cuda-nvrtc=11.8 cuda-nvrtc-dev=11.8 cuda-driver-dev=11.8 \
-                      cuda-nvcc=11.8 cuda-cccl=11.8 cuda-runtime=11.8 cuda-version=11.8 \
-                      libcusparse=11 libcusparse-dev=11 libcublas=11 libcublas-dev=11 \
+                      cuda-nvcc=11.8 cuda-cccl=11.8 cuda-runtime=11.8 \
                       -c nvidia -c pytorch -y
 pip list; echo ==========; conda --version; echo ==========; conda list
 
 # 4. Install the required packages
 pip install -r requirements.txt
 export CUDA_HOME=""; DS_BUILD_FUSED_ADAM=1 pip install --no-cache deepspeed; ds_report
-MAX_JOBS=40 pip install --no-cache --no-build-isolation --upgrade flash-attn;  # for Micorsoft's Phi models
+MAX_JOBS=40 pip install --no-cache --no-build-isolation --upgrade flash-attn;  # for Micorsoft's models
 rm -rf transformers; git clone https://github.com/chrisjihee/transformers.git; pip install -U -e transformers
 rm -rf chrisbase;    git clone https://github.com/chrisjihee/chrisbase.git;    pip install -U -e chrisbase
 rm -rf chrisdata;    git clone https://github.com/chrisjihee/chrisdata.git;    pip install -U -e chrisdata
 rm -rf progiter;     git clone https://github.com/chrisjihee/progiter.git;     pip install -U -e progiter
-pip list | grep -E "torch|lightn|trans|accel|speed|flash|numpy|piece|eval|chris|prog|pydantic"
+pip list | grep -E "torch|lightn|trans|accel|speed|flash|numpy|piece|eval|chris|prog|pydantic" > dep_versions.txt
 
 # 5. Unzip some archived data
 cd data; gzip -d -k pile-ner.json.gz; cd ..
