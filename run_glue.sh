9d64215993adf473479cd3f8e5672119ec0cdbdb
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-06 16:21:56 +0900

    Update run_glue.sh

--- run_glue.sh
+++ run_glue.sh
@@ -1,12 +1,14 @@
 export TASK_NAME=stsb
 
-python run_glue.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_glue.py \
   --model_name_or_path google-bert/bert-base-cased \
   --task_name $TASK_NAME \
   --do_train \
   --do_eval \
   --max_seq_length 128 \
-  --per_device_train_batch_size 32 \
+  --per_device_train_batch_size 4 \
   --learning_rate 2e-5 \
   --num_train_epochs 3 \
-  --output_dir output/$TASK_NAME/
+  --save_strategy epoch \
+  --eval_strategy epoch \
+  --output_dir output/$TASK_NAME
