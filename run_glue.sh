61bf47f0e0d43076e835fba74248151a36e1eff6
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-06 16:44:30 +0900

    Update run_glue.sh

--- run_glue.sh
+++ run_glue.sh
@@ -4,10 +4,13 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_glue.py \
   --task_name stsb \
   --do_train \
   --do_eval \
+  --bf16 \
+  --bf16_full_eval \
   --max_seq_length 128 \
+  --per_device_eval_batch_size 4 \
   --per_device_train_batch_size 4 \
   --learning_rate 2e-5 \
   --num_train_epochs 3 \
-  --save_strategy epoch \
+  --save_strategy no \
   --eval_strategy epoch \
   --output_dir output/stsb
