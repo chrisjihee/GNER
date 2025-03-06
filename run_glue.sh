ba3edc4e7ed782c3722da46aabb563bc8c1c1e1b
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-06 19:03:27 +0900

    Update run_glue.sh for 10 models

--- run_glue.sh
+++ run_glue.sh
@@ -1,16 +1,44 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_glue.py \
-  --model_name_or_path google-bert/bert-base-cased \
-  --task_name stsb \
-  --do_train \
-  --do_eval \
-  --bf16 \
-  --bf16_full_eval \
-  --max_seq_length 256 \
-  --per_device_eval_batch_size 4 \
-  --per_device_train_batch_size 4 \
-  --learning_rate 2e-5 \
-  --num_train_epochs 3 \
-  --save_strategy no \
-  --eval_strategy epoch \
-  --overwrite_output_dir \
-  --output_dir output/stsb
+#!/usr/bin/env bash
+set -x
+
+CUDA_DEVICES=0
+PROGRAM_SOURCE="run_glue.py"
+DEEPSPEED_PORT=$(shuf -i 25000-30000 -n 1)
+DEEPSPEED_CONFIG="configs/deepspeed/ds0_t5.json"
+
+MODELS=(
+  "google-bert/bert-base-cased"
+  "google-bert/bert-large-cased"
+  "FacebookAI/roberta-base"
+  "FacebookAI/roberta-large"
+  "FacebookAI/xlm-roberta-base"
+  "FacebookAI/xlm-roberta-large"
+  "microsoft/deberta-v3-base"
+  "microsoft/deberta-v3-large"
+  "answerdotai/ModernBERT-base"
+  "answerdotai/ModernBERT-large"
+)
+
+for MODEL_NAME_OR_PATH in "${MODELS[@]}"; do
+  MODEL_OUT_DIR=$(echo "$MODEL_NAME_OR_PATH" | tr '/' '-')
+
+  python -m deepspeed.launcher.runner --include="localhost:$CUDA_DEVICES" --master_port "$DEEPSPEED_PORT" "$PROGRAM_SOURCE" \
+    --model_name_or_path "$MODEL_NAME_OR_PATH" \
+    --task_name stsb \
+    --do_train \
+    --do_eval \
+    --bf16 True \
+    --tf32 True \
+    --max_seq_length 256 \
+    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 32 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3 \
+    --logging_strategy epoch \
+    --eval_strategy epoch \
+    --save_strategy no \
+    --deepspeed "$DEEPSPEED_CONFIG" \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --output_dir "output/stsb/$MODEL_OUT_DIR"
+done
