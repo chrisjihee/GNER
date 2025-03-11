0e5c4fb9656f30549925d03e4e0492bf2e7fe670
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-11 23:32:05 +0900

    Update rerank_by_predict_results(): complete code!

--- generate.py
+++ generate.py
@@ -238,7 +238,7 @@ def rerank_by_predict_results(
         generation_file: Annotated[str, typer.Option("--generation_file")] = "output/ZSE-predict/ZSE-validation-pred-by_beam-num=100.jsonl",  # "output/ZSE-predict/ZSE-test-pred-by_beam-num=100.jsonl", "output/ZSE-predict/ZSE-validation-pred-by_beam-num=100.jsonl"
         regression_input_files: Annotated[str, typer.Option("--regression_input_files")] = "data/GNER-QE/ZSE-validation-pred-by_beam-num=*-val.json",
         regression_output_files: Annotated[str, typer.Option("--regression_output_files")] = "output/GNER-QE/**/checkpoint-*-pred/predict_results_*",
-        pretrained: Annotated[str, typer.Option("--pretrained")] = "dyyyyyyyy/GNER-T5-base",  # "dyyyyyyyy/GNER-T5-large", "output-lfs/ZSE-jihee-BL-dl012/FlanT5-Base-BL/checkpoint-9900", "output-lfs/ZSE-yuyang-BL-lirs-b1/checkpoint-9900"
+        # pretrained: Annotated[str, typer.Option("--pretrained")] = "dyyyyyyyy/GNER-T5-base",  # "dyyyyyyyy/GNER-T5-large", "output-lfs/ZSE-jihee-BL-dl012/FlanT5-Base-BL/checkpoint-9900", "output-lfs/ZSE-yuyang-BL-lirs-b1/checkpoint-9900"
         output_home: Annotated[str, typer.Option("--output_home")] = "output",
         output_name: Annotated[str, typer.Option("--output_name")] = "ZSE-rerank",
         output_file: Annotated[str, typer.Option("--output_file")] = "rerank.jsonl",
@@ -261,7 +261,7 @@ def rerank_by_predict_results(
     env.setup_logger(env.logging_level)
 
     with (JobTimer(f"python {env.current_file} {' '.join(env.command_args)}", rt=1, rb=1, rc='=', verbose=verbose)):
-        tokenizer = AutoTokenizer.from_pretrained(pretrained)
+        # tokenizer = AutoTokenizer.from_pretrained(pretrained)
         gner_data = defaultdict(GenNERSampleWrapper)
         with Path(generation_file).open() as generation_data:
             for line in generation_data:
@@ -285,7 +285,6 @@ def rerank_by_predict_results(
         for model in regression_outputs:
             logger.info(f"[model] {model}")
             for regression_output_file in regression_outputs[model]:
-                # logger.info(f"- [regression_output_file] {regression_output_file}")
                 regression_input_name = regression_output_file.stem.split("predict_results_")[-1]
                 regression_input_samples = regression_inputs[regression_input_name]
                 regression_output_labels = pd.read_csv(regression_output_file, delimiter="\t")['prediction'].tolist()
@@ -314,9 +313,7 @@ def rerank_by_predict_results(
                 with reranked_data_file.open("w") as out:
                     for example in reranked_gner_data:
                         out.write(example.model_dump_json() + "\n")
-                # logger.info(f"  => {reranked_data_file}")
                 logger.info(f"- {regression_output_file} => {reranked_data_file}")
-            exit(1)
 
 
 @main.command("check_possibility")
