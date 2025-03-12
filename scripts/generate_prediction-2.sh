00b5555a0bcfe742372da4ed3423c6b2b4362378
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-12 15:28:59 +0900

    Update minor

--- scripts/generate_prediction-2.sh
+++ scripts/generate_prediction-2.sh
@@ -1,1 +1,1 @@
-python generate.py generate_prediction --device=cuda:2 --input_file data/pile-ner=10-100,3-10,3-10.jsonl --num_generation 100 --generation_by_sample --temperature=1.5 --top_p=0.9
+python generate.py generate_prediction --device=cuda:2 --input_file data/pile-ner=10-100,3-10,3-10.jsonl --num_generation 100 --generation_by_sample
