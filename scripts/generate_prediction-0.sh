308e24c618f25aaddd8324e9ec7db40950254634
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-12 16:16:03 +0900

    --num_generation 120 --generation_by_sample & --num_generation 80 --generation_by_beam

--- scripts/generate_prediction-0.sh
+++ scripts/generate_prediction-0.sh
@@ -1,1 +1,1 @@
-python generate.py generate_prediction --device=cuda:0 --input_file data/pile-ner=10-100,3-7,3-10.jsonl --num_generation 100 --generation_by_sample
+python generate.py generate_prediction --device=cuda:0 --input_file data/pile-ner=10-100,3-7,3-10.jsonl --num_generation 120 --generation_by_sample
--- scripts/generate_prediction-1.sh
+++ scripts/generate_prediction-1.sh
@@ -1,1 +1,1 @@
-python generate.py generate_prediction --device=cuda:1 --input_file data/pile-ner=10-100,3-7,3-10.jsonl --num_generation 70 --generation_by_beam
+python generate.py generate_prediction --device=cuda:1 --input_file data/pile-ner=10-100,3-7,3-10.jsonl --num_generation 80 --generation_by_beam
