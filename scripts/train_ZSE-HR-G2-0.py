4d3f144d9d1b5e282ee3bda576d80f9d74977ef9
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-15 07:35:59 +0900

    set train_epochs per model!

--- scripts/train_ZSE-HR-G2-0.py
+++ scripts/train_ZSE-HR-G2-0.py
@@ -22,7 +22,6 @@ pred_file = "data/ZSE-test-sampled-N70-HR1500,700,800.jsonl"
 metric_for_best_model = "eval_average"
 max_generation_tokens = 640
 save_total_limit = 3
-train_epochs = 1
 eval_epochs = 0.5
 save_epochs = eval_epochs
 learning_rate = 5e-5
@@ -43,6 +42,7 @@ for spec in model_specs_2gpu:
     suffix = f"-{experiment_type}"
     run_version = f"{spec['run_prefix']}{suffix}"
     train_batch = spec['train_batch']
+    train_epochs = 1  # spec['train_epochs']
     gradient_steps = int(total_batch / num_devices / train_batch)
     assert gradient_steps * train_batch * num_devices == total_batch, f"total_batch={total_batch} != gradient_steps={gradient_steps} * train_batch={train_batch} * num_devices={num_devices}"
     use_flash_attention = spec['pretrained'].startswith("microsoft/Phi")
--- scripts/train_ZSE-HR-G4b-4.py
+++ scripts/train_ZSE-HR-G4b-4.py
@@ -22,7 +22,6 @@ pred_file = "data/ZSE-test-sampled-N700-HR10100,2100,8000.jsonl"
 metric_for_best_model = "eval_average"
 max_generation_tokens = 640
 save_total_limit = 3
-train_epochs = 6
 eval_epochs = 0.5
 save_epochs = eval_epochs
 learning_rate = 5e-5
@@ -43,6 +42,7 @@ for spec in model_specs_4gpu_b:
     suffix = f"-{experiment_type}"
     run_version = f"{spec['run_prefix']}{suffix}"
     train_batch = spec['train_batch']
+    train_epochs = spec['train_epochs']
     gradient_steps = int(total_batch / num_devices / train_batch)
     assert gradient_steps * train_batch * num_devices == total_batch, f"total_batch={total_batch} != gradient_steps={gradient_steps} * train_batch={train_batch} * num_devices={num_devices}"
     use_flash_attention = spec['pretrained'].startswith("microsoft/Phi")
