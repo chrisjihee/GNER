69049cdcda12f9faec5b98e871daed5189fafe9a
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-15 07:04:19 +0900

    train_batch = 8 for H100

--- scripts/base.py
+++ scripts/base.py
@@ -13,8 +13,8 @@ model_specs_4gpu_a = [
 ]
 
 model_specs_4gpu_b = [
-    {"ds_config": "configs/deepspeed/ds2_llama.json", "run_prefix": "Phi3-4B", "pretrained": "microsoft/Phi-3-mini-4k-instruct", "train_batch": 4},  # modeling_phi3.py: get_max_length -> get_max_cache_shape
-    {"ds_config": "configs/deepspeed/ds2_llama.json", "run_prefix": "Qwen2-3B", "pretrained": "Qwen/Qwen2.5-3B", "train_batch": 4},
+    {"ds_config": "configs/deepspeed/ds2_llama.json", "run_prefix": "Phi3-4B", "pretrained": "microsoft/Phi-3-mini-4k-instruct", "train_batch": 8},  # modeling_phi3.py: get_max_length -> get_max_cache_shape
+    {"ds_config": "configs/deepspeed/ds2_llama.json", "run_prefix": "Qwen2-3B", "pretrained": "Qwen/Qwen2.5-3B", "train_batch": 8},
 ]
 
 model_specs_8gpu = [
