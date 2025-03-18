8962117cfeea3ba17271181a7cb3ccf79ea41799
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-19 01:26:11 +0900

    #--do_check_possibility

--- generate_prediction-15.sh
+++ generate_prediction-15.sh
@@ -1,1 +1,1 @@
-python generate.py generate_hybrid_prediction --device cuda:0 --input_file data/pile-ner-sampled-N19988.jsonl --input_start 14000 --input_limit 1000 --do_check_possibility
+python generate.py generate_hybrid_prediction --device cuda:0 --input_file data/pile-ner-sampled-N19988.jsonl --input_start 14000 --input_limit 1000 #--do_check_possibility
