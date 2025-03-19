47ef66abaf8b944ede94fe5946d86293d572e570
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-19 10:54:26 +0900

    Update --device

--- generate_prediction-20.sh
+++ generate_prediction-20.sh
@@ -1,1 +1,1 @@
-python generate.py generate_hybrid_prediction --device cuda:1 --input_file data/pile-ner-sampled-N19988.jsonl --input_start 19000 --input_limit 1000 #--do_check_possibility
+python generate.py generate_hybrid_prediction --device cuda:3 --input_file data/pile-ner-sampled-N19988.jsonl --input_start 19000 --input_limit 1000 #--do_check_possibility
