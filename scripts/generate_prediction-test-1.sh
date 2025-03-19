c13af7da2c2b135044bc58eaf8fc2310e4c1a1be
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-19 16:15:50 +0900

    Add generate_prediction-test-1.sh, generate_prediction-val-1.sh

---
+++ scripts/generate_prediction-test-1.sh
@@ -0,0 +1,1 @@
+python generate.py generate_hybrid_prediction --device cuda:6 --input_file data/ZSE-test-sampled-N700.jsonl --do_check_possibility
---
+++ scripts/generate_prediction-val-1.sh
@@ -0,0 +1,1 @@
+python generate.py generate_hybrid_prediction --device cuda:5 --input_file data/ZSE-validation-sampled-N210.jsonl --do_check_possibility
