66202793912cc0b47a9658e972aa20f44bda6a19
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-20 22:24:57 +0900

    Update run-train_GNER-QE.sh: change train setting!

--- run-train_GNER-QE.sh
+++ run-train_GNER-QE.sh
@@ -3,17 +3,17 @@ set -x
 
 DEEPSPEED_CONFIG="configs/deepspeed/ds1_t5.json"
 DEEPSPEED_PORT=$(shuf -i 25000-30000 -n 1)
-CUDA_DEVICES=4,5,6,7
+CUDA_DEVICES=0,1,2,3
 SOURCE_FILE="run_glue.py"
 TRAIN_POSTFIX="max_sampled=3"
-TRAIN_FILE="data/GNER-QE/pile-ner-sampled-N19988-quality_est-max_sampled=3.json"
+TRAIN_FILE="data/GNER-QE/pile-ner-sampled-N19988-quality_est-${TRAIN_POSTFIX}.json"
 VALID_FILE="data/GNER-QE/ZSE-validation-sampled-N210-quality_est-max_sampled=10.json"
 TEST_FILE="data/GNER-QE/ZSE-test-sampled-N700-quality_est-max_sampled=0.json"
 OUTPUT_NAME="GNER-QE-HR"
 OUTPUT_HOME="output-lfs"
 
 MODEL_NAMES=(
-#  "FacebookAI/roberta-base"
+  "FacebookAI/roberta-base"
 #  "FacebookAI/roberta-large"
   "microsoft/deberta-v3-base"
 #  "microsoft/deberta-v3-large"
@@ -37,14 +37,17 @@ for MODEL_NAME in "${MODEL_NAMES[@]}"; do
       --bf16 True \
       --tf32 True \
       --max_seq_length 512 \
-      --per_device_eval_batch_size 16 \
-      --per_device_train_batch_size 4 \
-      --gradient_accumulation_steps 4 \
+      --per_device_eval_batch_size 64 \
+      --per_device_train_batch_size 64 \
+      --gradient_accumulation_steps 1 \
       --learning_rate 2e-5 \
-      --num_train_epochs 40 \
-      --logging_strategy epoch \
-      --eval_strategy epoch \
-      --save_strategy epoch \
+      --num_train_epochs 20 \
+      --logging_strategy steps \
+      --eval_strategy steps \
+      --save_strategy steps \
+      --logging_steps 10 \
+      --eval_steps 315 \
+      --save_steps 315 \
       --save_total_limit 3 \
       --load_best_model_at_end True \
       --metric_for_best_model pearson \
