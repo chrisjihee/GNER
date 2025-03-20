ee581d98563c1bf9fc0be0b06103aa8670783ec9
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-20 16:48:15 +0900

    Update run-train_GNER-QE.sh

--- run-train_GNER-QE.sh
+++ run-train_GNER-QE.sh
@@ -3,19 +3,18 @@ set -x
 
 DEEPSPEED_CONFIG="configs/deepspeed/ds1_t5.json"
 DEEPSPEED_PORT=$(shuf -i 25000-30000 -n 1)
-CUDA_DEVICES=0
+CUDA_DEVICES=6,7
 SOURCE_FILE="run_glue.py"
-TRAIN_FILE="data/GNER-QE/ZSE-validation-pred-by_beam-num=10-train.json"
-VALID_FILE="data/GNER-QE/ZSE-validation-pred-by_beam-num=10-val.json"
-OUTPUT_NAME="GNER-QE"
+TRAIN_FILE="data/GNER-QE/pile-ner-sampled-N19988-quality_est-max_sampled=3.jsonl"
+VALID_FILE="data/GNER-QE/ZSE-validation-sampled-N210-quality_est-max_sampled=10.jsonl"
+TEST_FILE="data/GNER-QE/ZSE-test-sampled-N700-quality_est-max_sampled=0.jsonl"
+OUTPUT_NAME="GNER-QE-HR"
+OUTPUT_HOME="output"
 
 MODEL_NAMES=(
-#  "google-bert/bert-base-cased"
 #  "FacebookAI/roberta-base"
 #  "FacebookAI/roberta-large"
-  "answerdotai/ModernBERT-base"
-  "answerdotai/ModernBERT-large"
-#  "microsoft/deberta-v3-base"
+  "microsoft/deberta-v3-base"
 #  "microsoft/deberta-v3-large"
 )
 
@@ -27,15 +26,17 @@ for MODEL_NAME in "${MODEL_NAMES[@]}"; do
     $SOURCE_FILE \
       --train_file $TRAIN_FILE \
       --validation_file $VALID_FILE \
+      --test_file $TEST_FILE \
       --model_name_or_path $MODEL_NAME \
-      --output_dir output/$OUTPUT_NAME/$MODEL_NAME-num=10 \
+      --output_dir $OUTPUT_HOME/$OUTPUT_NAME/$MODEL_NAME-num=10 \
       --cache_dir .cache \
       --do_train \
       --do_eval \
+      --do_predict \
       --bf16 True \
       --tf32 True \
       --max_seq_length 512 \
-      --per_device_eval_batch_size 4 \
+      --per_device_eval_batch_size 16 \
       --per_device_train_batch_size 4 \
       --gradient_accumulation_steps 4 \
       --learning_rate 2e-5 \
@@ -45,7 +46,7 @@ for MODEL_NAME in "${MODEL_NAMES[@]}"; do
       --save_strategy epoch \
       --save_total_limit 3 \
       --load_best_model_at_end True \
-      --metric_for_best_model spearmanr \
+      --metric_for_best_model pearson \
       --deepspeed $DEEPSPEED_CONFIG \
       --overwrite_output_dir \
       --overwrite_cache
