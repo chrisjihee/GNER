73341206484293663e932e069bcb64183be5f02d
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-21 16:43:51 +0900

    Update run-predict_GNER-QE.py: for GNER-QE-HR

--- run-predict_GNER-QE.py
+++ run-predict_GNER-QE.py
@@ -6,49 +6,45 @@ from chrisbase.io import dirs, files
 
 DEEPSPEED_CONFIG = "configs/deepspeed/ds3_t5.json"
 DEEPSPEED_PORT = random.randint(25000, 30000)
-CUDA_DEVICES = os.getenv("CUDA_VISIBLE_DEVICES", "0")
+CUDA_DEVICES = os.getenv("CUDA_VISIBLE_DEVICES", "1")
 SOURCE_FILE = "run_glue.py"
 
-trained_model_paths = dirs("output/GNER-QE/**/bert-base-*/checkpoint-*")
-train_files = files("data/GNER-QE/ZSE-validation-pred-by_beam-num=*-train.json")
-valid_files = files("data/GNER-QE/ZSE-validation-pred-by_beam-num=*-val.json")
-test_files = files("data/GNER-QE/ZSE-test-pred-by_beam-num=*-test.json")
-assert len(train_files) == len(valid_files) == len(test_files), f"train_files: {len(train_files)}, valid_files: {len(valid_files)}, test_files: {len(test_files)}"
+trained_model_paths = dirs("output-lfs/GNER-QE-HR-*/**/roberta-*/checkpoint-*")
+# trained_model_paths = dirs("output-lfs/GNER-QE-HR-*/**/deberta-*/checkpoint-*")
+TRAIN_DATA_POST = "max_sampled=3"
+TRAIN_FILE = f"data/GNER-QE/pile-ner-sampled-N19988-quality_est-${TRAIN_DATA_POST}.json"
+VALID_FILE = "data/GNER-QE/ZSE-validation-sampled-N210-quality_est-max_sampled=0.json"
+TEST_FILE = "data/GNER-QE/ZSE-test-sampled-N700-quality_est-max_sampled=0.json"
 
 for model_path in trained_model_paths:
-    for train_file, valid_file, test_file in zip(train_files, valid_files, test_files):
-        command = f"""
-            python -m
-                deepspeed.launcher.runner
-                    --include=localhost:{CUDA_DEVICES}
-                    --master_port={DEEPSPEED_PORT}
-                {SOURCE_FILE}
-                    --train_file {train_file}
-                    --validation_file {valid_file}
-                    --test_file {test_file}
-                    --model_name_or_path {model_path}
-                    --output_dir {model_path}/pred
-                    --cache_dir .cache
-                    --do_predict
-                    --bf16 True
-                    --tf32 True
-                    --max_seq_length 512
-                    --per_device_eval_batch_size 16
-                    --per_device_train_batch_size 4
-                    --gradient_accumulation_steps 4
-                    --learning_rate 2e-5
-                    --num_train_epochs 40
-                    --logging_strategy epoch
-                    --eval_strategy epoch
-                    --save_strategy epoch
-                    --deepspeed {DEEPSPEED_CONFIG}
-                    --overwrite_output_dir
-                    --overwrite_cache
-        """
-        command = command.strip().split()
-        print("*" * 120)
-        print("[COMMAND]", " ".join(command))
-        print("*" * 120)
+    command = f"""
+        python -m
+            deepspeed.launcher.runner
+                --include=localhost:{CUDA_DEVICES}
+                --master_port={DEEPSPEED_PORT}
+            {SOURCE_FILE}
+                --train_file {TRAIN_FILE}
+                --validation_file {VALID_FILE}
+                --test_file {TEST_FILE}
+                --model_name_or_path {model_path}
+                --output_dir {model_path}/pred
+                --cache_dir .cache
+                --do_predict
+                --bf16 True --tf32 True
+                --max_seq_length 640
+                --per_device_eval_batch_size 30
+                --per_device_train_batch_size 16
+                --gradient_accumulation_steps 1
+                --deepspeed {DEEPSPEED_CONFIG}
+                --logging_strategy epoch
+                --eval_strategy epoch
+                --save_strategy epoch
+                --overwrite_output_dir
+    """
+    command = command.strip().split()
+    print("*" * 120)
+    print("[COMMAND]", " ".join(command))
+    print("*" * 120)
 
-        subprocess.run(command)
-        print("\n" * 3)
+    subprocess.run(command)
+    print("\n" * 3)
